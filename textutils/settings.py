f05422c39ad314883f8ebc52b556bffa05843261
omondi <onyangodonomondi@gmail.com>
2024-09-04 23:15:02 +0300

    static

--- textutils/settings.py
+++ textutils/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # Add this line
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files (for handling uploaded images like profile pictures)
 MEDIA_URL = '/media/'
