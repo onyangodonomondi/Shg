56d96d68afeb2d8c9140e793a3e89dfe3e6e0748
omondi <onyangodonomondi@gmail.com>
2024-09-05 20:38:05 +0300

    login

--- members/signals.py
+++ members/signals.py
@@ -6,12 +6,8 @@ from .models import Profile
 @receiver(post_save, sender=User)
 def create_or_update_profile(sender, instance, created, **kwargs):
     if created:
-        # Only create the profile if it doesn't already exist
-        Profile.objects.get_or_create(user=instance)
+        # Create a profile for a newly created user
+        Profile.objects.create(user=instance)
     else:
-        # Update the profile if the user is updated
-        try:
-            instance.profile.save()
-        except Profile.DoesNotExist:
-            # If the profile doesn't exist (unlikely), create it
-            Profile.objects.create(user=instance)
+        # Update the profile if needed; typically, this shouldn't be necessary for OneToOneField
+        instance.profile.save()
