93b096172aa64d8317d1fdf27dab5b5b39c7f01e
omondi <onyangodonomondi@gmail.com>
2024-09-07 21:52:31 +0300

    update

--- members/admin.py
+++ members/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-from .models import Profile, Event, Contribution
+from .models import Profile, Event, Contribution, EventCategory
+from django.contrib.admin import SimpleListFilter
+from django.db import models
+
 
 # Inline admin for Profile
 class ProfileInline(admin.StackedInline):
@@ -11,19 +14,51 @@ class ProfileInline(admin.StackedInline):
     fk_name = 'user'
     fields = ('othernames', 'email', 'phone_number', 'birthdate', 'has_children', 'number_of_children', 'image')
 
-# Customized User admin
+# Custom filter for Contribution categories
+class CategoryFilter(SimpleListFilter):
+    title = 'Category'  # The title displayed in the filter dropdown
+    parameter_name = 'category'  # The URL query parameter used for the filter
+
+    def lookups(self, request, model_admin):
+        """Return a list of options for the filter."""
+        return [
+            ('Fully Contributed', 'Fully Contributed'),
+            ('Partially Contributed', 'Partially Contributed'),
+            ('No Contribution', 'No Contribution'),
+        ]
+
+    def queryset(self, request, queryset):
+        """Return the filtered queryset based on the selected filter option."""
+        value = self.value()
+        if value == 'Fully Contributed':
+            return queryset.filter(amount__gte=models.F('event__required_amount'))
+        elif value == 'Partially Contributed':
+            return queryset.filter(amount__gt=0, amount__lt=models.F('event__required_amount'))
+        elif value == 'No Contribution':
+            return queryset.filter(amount=0)
+        return queryset
+
+# Customized User admin with profile details
 class UserAdmin(BaseUserAdmin):
     inlines = (ProfileInline,)
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'get_phone_number', 'get_other_names')
     search_fields = ('username', 'first_name', 'last_name', 'email')
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+
+    def get_phone_number(self, obj):
+        return obj.profile.phone_number
+    get_phone_number.short_description = 'Phone Number'
+
+    def get_other_names(self, obj):
+        return obj.profile.othernames
+    get_other_names.short_description = 'Other Names'
 
     def get_inline_instances(self, request, obj=None):
         if not obj:
             return []
         return super(UserAdmin, self).get_inline_instances(request, obj)
 
-# Re-register UserAdmin
+# Re-register UserAdmin with custom profile inline
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 
@@ -40,6 +75,14 @@ class EventAdmin(admin.ModelAdmin):
         }),
     )
 
+    # Custom action to mark events as inactive
+    actions = ['mark_inactive']
+
+    def mark_inactive(self, request, queryset):
+        queryset.update(is_active=False)
+        self.message_user(request, "Selected events have been marked as inactive.")
+    mark_inactive.short_description = 'Mark selected events as inactive'
+
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         return qs.order_by('date')
@@ -49,13 +92,15 @@ class EventAdmin(admin.ModelAdmin):
 class ContributionAdmin(admin.ModelAdmin):
     list_display = ('get_profile_name', 'event', 'amount', 'category')
     search_fields = ('profile__user__first_name', 'profile__user__last_name', 'event__name')
-    list_filter = ('event', 'profile__user__first_name', 'profile__user__last_name')
+    list_filter = ('event', 'profile__user__first_name', 'profile__user__last_name', CategoryFilter)  # Use the custom CategoryFilter class here
     ordering = ('event', 'profile__user__last_name')
     readonly_fields = ('category',)
 
     def get_profile_name(self, obj):
         return f"{obj.profile.user.first_name} {obj.profile.user.last_name}"
     get_profile_name.short_description = 'Profile Name'
+
+
 
 # Customizing the admin site titles
 admin.site.site_header = "NYAGWA SHG Administration"
--- members/forms.py
+++ members/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile, Contribution, Event
+
+# User Sign-Up Form
 class UserSignUpForm(UserCreationForm):
     email = forms.EmailField(required=True)
     first_name = forms.CharField(max_length=30, required=True)
@@ -25,6 +27,8 @@ class UserSignUpForm(UserCreationForm):
             user.profile.save()
         return user
 
+
+# User Update Form
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField(required=True)
     first_name = forms.CharField(max_length=30, required=True)
@@ -34,22 +38,44 @@ class UserUpdateForm(forms.ModelForm):
         model = User
         fields = ['username', 'email', 'first_name', 'last_name']
 
+
+# Profile Update Form (with lineage fields)
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['image', 'bio', 'location', 'birthdate', 'phone_number']
+        fields = ['image', 'bio', 'location', 'birthdate', 'phone_number', 'father', 'mother', 'has_children', 'number_of_children']
         widgets = {
             'bio': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
             'location': forms.TextInput(attrs={'class': 'form-control'}),
             'birthdate': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
             'phone_number': forms.TextInput(attrs={'class': 'form-control'}),
             'image': forms.ClearableFileInput(attrs={'class': 'form-control-file'}),
+            'father': forms.Select(attrs={'class': 'form-control'}),  # Dropdown for selecting father
+            'mother': forms.Select(attrs={'class': 'form-control'}),  # Dropdown for selecting mother
+            'has_children': forms.CheckboxInput(attrs={'class': 'form-check-input'}),  # Checkbox for has_children
+            'number_of_children': forms.NumberInput(attrs={'class': 'form-control', 'min': 0})  # Input for number_of_children
         }
+
+    def clean_number_of_children(self):
+        """Validate number_of_children based on the value of has_children"""
+        has_children = self.cleaned_data.get('has_children')
+        number_of_children = self.cleaned_data.get('number_of_children')
+
+        if has_children and (number_of_children is None or number_of_children < 1):
+            raise forms.ValidationError("Please specify the number of children.")
+        elif not has_children:
+            return 0  # Return 0 if user has no children
+        return number_of_children
+
+
+# Event Form
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
         fields = ['name', 'date', 'required_amount', 'is_active']
 
+
+# Contribution Form
 class ContributionForm(forms.ModelForm):
     class Meta:
         model = Contribution
